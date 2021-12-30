4a753ecd474573dcbed7f6deaef2b13298298d11
Josh Moody <jgmoody@gmail.com>
2021-12-30 12:07:16 -0600

    Added table indexes. Set more reasonable field lengths.

--- build_sqlite.php
+++ build_sqlite.php
@@ -15,18 +15,33 @@ $word_stmt = $pdo->prepare($word_sql);
 $meaning_sql = "INSERT INTO meanings (id, wordset_id, definition, example, speech_part) VALUES (:id, :wordset_id, :definition, :example, :speech_part)";
 $meaning_stmt = $pdo->prepare($meaning_sql);
 
-foreach(glob("$data_path/*.json") as $letter){
+$max_word_length = 0;
+$max_definition_length = 0;
+$word_count = 0;
 
-	if ($letter == 'misc.json'){
+foreach(glob("$data_path/*.json") as $letter){
+	$source = basename($letter);
+
+	if ($source == 'misc.json'){
 		continue;
 	}
+
+	print "Processing $source" . PHP_EOL;
+
 	$words = json_decode(file_get_contents($letter));
 
 	foreach($words as $word){
+		$word_count++;
 		$word_stmt->execute([
 			'wordset_id' => $word->wordset_id,
-			'word' => $word->word
+			'word' => trim($word->word)
 		]);
+
+		$word_length = strlen($word->word);
+
+		if ($word_length > $max_word_length){
+			$max_word_length = $word_length;
+		}
 
 		if (empty($word->meanings)){
 			continue;
@@ -40,6 +55,15 @@ foreach(glob("$data_path/*.json") as $letter){
 				'example' => $meaning->example ?? null,
 				'speech_part' => $meaning->speech_part
 			]);
+
+			$definition_length = strlen($meaning->def);
+			if ($definition_length > $max_definition_length){
+				$max_definition_length = $definition_length;
+			}
 		}
 	}
 }
+
+print "Number of Words: $word_count" . PHP_EOL;
+print "Max Word Length: $max_word_length" . PHP_EOL;
+print "Max Definition Length: $max_definition_length" . PHP_EOL;
--- shema.sql
+++ shema.sql
@@ -1,15 +1,22 @@
 CREATE TABLE IF NOT EXISTS "words" (
     "wordset_id" varchar(10) NOT NULL,
-    "word" varchar(255)
+    "word" nvarchar(100)
     );
+
+CREATE INDEX IF NOT EXISTS WordsWordsetIdIndex ON "words" ("wordset_id" ASC);
+CREATE INDEX IF NOT EXISTS WordsWordIndex ON "words" ("word" ASC);
 
 CREATE TABLE IF NOT EXISTS "meanings" (
     "id" varchar(10) NOT NULL,
     "wordset_id" varchar(10),
-    "definition" text(128),
-    "example" text(128),
+    "definition" text(500),
+    "example" text(500),
     "speech_part" varchar(20)
     );
+
+CREATE INDEX IF NOT EXISTS MeaningsIdIndex ON "meanings" ("id" ASC);
+CREATE INDEX IF NOT EXISTS MeaningsWordsetIdIndex ON "meanings" ("wordset_id" ASC);
+CREATE INDEX IF NOT EXISTS MeaningsSpeechPartIndex ON "meanings" ("speech_part" ASC);
 
 CREATE VIEW dictionary AS
 SELECT words.wordset_id, words.word, meanings.id, meanings.definition, meanings.example, meanings.speech_part
